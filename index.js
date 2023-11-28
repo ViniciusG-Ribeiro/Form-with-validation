8280753bac73a82e9f7e0206a72c965db8c7dbd2
ViniciusG.Ribeiro <viniciusrib2014@hotmail.com>
2023-11-28 20:31:41 -0300

    adicionando projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll(".entrada");
+
+const divInputs = document.querySelectorAll(".div-inputs");
+
+const errorText = document.getElementsByClassName(".error-text");
+
+function toSend(){
+
+    var cont = 0;
+
+    divInputs.forEach(function (item) {
+        
+        if(divInputs[cont].children.item(0).value == ""){
+            divInputs[cont].children.item(0).classList.add("input-null");
+            divInputs[cont].children.item(1).classList.add("error-text-ativo");
+            
+        }else{
+            divInputs[cont].children.item(0).classList.add("input-notnull");
+            divInputs[cont].children.item(0).classList.remove("input-null");
+            divInputs[cont].children.item(1).classList.remove("error-text-ativo");
+
+        }
+        cont++;
+    });
+}
